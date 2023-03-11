a918bb47bf4df9b5f314b9aed3a7f87402b11bf5
Владислав Фатихов <fatikhov.vl@icloud.com>
2023-03-11 04:21:30 +0300

    send in telegram

--- modalForm.js
+++ modalForm.js
@@ -1,52 +1,92 @@
+const successSendModal = document.getElementById('success-send-modal')
+const validSendModal = document.getElementById('valid-send-modal')
+const btnSendModal = document.getElementById('btn-send-modal')
+const loader = document.getElementById('loader-modal')
+const onSuccessModal = document.getElementById('onSuccess__modal')
+const btnSend = document.querySelector('.btn-send')
+
+const TOKEN = '5982848535:AAHG_jfr5wVA9lTRAxhSxYnuzdBT5pGB3JM'
+const CHATID = '459797765'
+
+let s = 'Заявка из формы'
+
+//блокируем кнопку после отправки
+function checkValidity(event) {
+  const formNode = event.target.form
+  const isValid = formNode.checkValidity()
+  btnSend.disabled = !isValid
+  if (!btnSend.disabled && btnSendModal.classList.contains('hidden')) {
+    return (
+      validSendModal.classList.toggle('hidden'),
+      btnSendModal.classList.toggle('hidden')
+    )
+  } else if (btnSend.disabled && validSendModal.classList.contains('hidden')) {
+    return (
+      validSendModal.classList.toggle('hidden'),
+      btnSendModal.classList.toggle('hidden')
+    )
+  }
+}
+
+//обработка ошибки
+function onError(error) {
+  alert(error.message)
+}
+
+//обрабатываем успешную отправку
+function onSuccess(formNode) {
+  formNode.classList.toggle('hidden')
+  onSuccessModal.classList.toggle('hidden')
+  btnSendModal.classList.toggle('hidden')
+  successSendModal.classList.toggle('hidden')
+  btnSend.disabled = 'disabled'
+}
+
 //Лоадер нажатой кнопки формы
 function toggleLoader() {
-  const btnSendModal = document.getElementById('btn-send-modal')
-  const loader = document.getElementById('loader-modal')
   btnSendModal.classList.toggle('hidden')
   loader.classList.toggle('hidden')
 }
 
 //отправка на сервер
 async function sendData(data) {
-  const TOKEN = '5982848535:AAHG_jfr5wVA9lTRAxhSxYnuzdBT5pGB3JM'
-  const CHATID = '459797765'
   return await fetch(
-    `https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHATID}&parse_mode=html&text=${data}`,
+    `https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHATID}&parse_mode=HTML&text=${data}`,
     {
       method: 'POST',
+      headers: { 'Content-Type': 'multipart/form-data' },
     }
   )
-    .then((res) => res.json())
-    .then((response) => {
-      if (!response.ok)
-        return console.log('ошибка', response.ok, response.result)
-      return console.log('Успешно', response.ok, response.result)
-    })
-    .catch((error) => {
-      console.error(error)
-    })
 }
 //сбор данных формы
 function serializeForm(formNode) {
   const data = new FormData(formNode)
-  console.log('22/', JSON.stringify(Array.from(data.entries())))
-  return JSON.stringify(Array.from(data.entries()))
+  Array.from(data.entries()).filter((e) => {
+    s += `  |---|  ${e[0]}: ${e[1]}`
+  })
+  return s
 }
 //кнопка нажата, вызываем функции: сбор данных формы и отправка на сервер
+// Вызовем её вот так:
 async function handleFormSubmit(event) {
   event.preventDefault()
   const data = serializeForm(event.target)
 
   toggleLoader()
 
-  const response = await sendData(data)
+  const { status } = await sendData(data)
 
-  setTimeout(() => {
-    toggleLoader()
-    //нужно закрыть модалку и показать модалку с результатом
-  }, 3000)
+  toggleLoader()
+
+  if (status === 200) {
+    onSuccess(event.target)
+  } else {
+    onError(error)
+  }
 }
 
-//слушаем клик по кнопке формы
 const applicantForm = document.getElementById('modal-form')
+//пока форма не заполнена, кнопка недоступна
+applicantForm.addEventListener('input', checkValidity)
+//слушаем клик по кнопке формы
 applicantForm.addEventListener('submit', handleFormSubmit)
